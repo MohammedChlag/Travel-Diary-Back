8b5b1945abf60ad41a97d6c5fc32cb2e1847694a
Mohammed-Chikri <mohammedchikrilag@icloud.com>
2025-01-28 22:03:56 +0100

    cntr z

--- src/models/users/selectUserByIdModel.js
+++ src/models/users/selectUserByIdModel.js
@@ -12,33 +12,11 @@ export const selectUserByIdModel = async (id) => {
   // Al hacer un select, el resultado es un array con 2 posiciones
   // La primera son los resultados. Es un array con tantas posiciones como registros haya encontrado.
   // La segunda es un array con información sobre la tabla
-  const [results] = await pool.query(
-    `
-		SELECT 
-			u.id, 
-			u.email, 
-			u.password, 
-			u.firstName, 
-			u.lastName, 
-			u.role, 
-			u.avatar, 
-			u.createdAt,
-			JSON_ARRAYAGG(
-				JSON_OBJECT(
-					'id', e.id,
-					'title', e.title,
-					'description', e.description,
-					'place', e.place,
-					'createdAt', e.createdAt
-				)
-			) as entries
-		FROM users u
-		LEFT JOIN entries e ON u.id = e.userId
-		WHERE u.id = ? AND u.active = 1
-		GROUP BY u.id`,
+  const [user] = await pool.query(
+    `SELECT id, email, password, firstName, lastName, role, avatar, createdAt FROM users WHERE id = ? AND active = 1`,
     [id]
   );
 
   // Como id es único, solo habrá un registro por lo que nos quedamos con la primera posición
-  return results[0];
+  return user[0];
 };
